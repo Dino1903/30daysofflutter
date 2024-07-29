b7c4e1eb5887dac2993254431ec714bb1dc0685e
Dino1903 <adeenahmahmood1@gmail.com>
2024-07-29 23:51:30 +0500

    day 2 third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(child: Text("welcome")),
-        ),
-      ),
+    
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
